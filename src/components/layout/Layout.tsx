f4bd718676d4b53bb1a467b7515d02b1ed978173
heshan <heshan.sanjaya@soundmouse.com>
2025-08-31 13:35:01 +0530

    add

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,9 +7,16 @@ interface LayoutProps {
 
 export function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen bg-background">
+    <div className="min-h-screen bg-background overflow-x-hidden">
       <Header />
-      <main className="container mx-auto p-4 sm:p-6 max-w-7xl">
+      <main
+        className="
+          mx-auto w-full
+          max-w-[1400px]
+          px-3 sm:px-4 lg:px-3 xl:px-4 2xl:px-5
+          py-4 sm:py-6
+        "
+      >
         {children}
       </main>
     </div>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,20 +2,52 @@ import { BorrowerPipeline } from "../components/borrower/BorrowerPipeline"
 import { BorrowerDetail } from "../components/borrower/BorrowerDetail"
 import { BrokerOverview } from "../components/broker/BrokerOverview"
 
+
 export function Dashboard() {
   return (
-    <div className="space-y-6 lg:space-y-0 lg:grid lg:grid-cols-12 lg:gap-6 h-auto lg:h-[calc(100vh-8rem)]">
-      <div className="lg:col-span-4 xl:col-span-3">
-        <BorrowerPipeline />
-      </div>
-      
-      <div className="lg:col-span-5 xl:col-span-6">
-        <BorrowerDetail />
-      </div>
-      
-      <div className="lg:col-span-3 xl:col-span-3">
-        <BrokerOverview />
-      </div>
+    <div
+      className="
+        w-full overflow-x-hidden box-border
+        flex flex-col gap-6
+        lg:grid
+        lg:[grid-template-columns:minmax(0,1fr)_minmax(0,1.25fr)_minmax(0,1fr)]
+        xl:[grid-template-columns:minmax(0,1fr)_minmax(0,2fr)_minmax(0,1fr)]
+        lg:gap-4 xl:gap-6
+        min-h-[calc(100vh-7rem)]
+      "
+    >
+      <section
+        className="
+          min-w-0 break-words
+          lg:max-h-[calc(100vh-10rem)] lg:overflow-hidden
+        "
+      >
+        <div className="h-full overflow-auto pr-1">
+          <BorrowerPipeline />
+        </div>
+      </section>
+
+      <section
+        className="
+          min-w-0 break-words
+          lg:max-h-[calc(100vh-10rem)] lg:overflow-hidden
+        "
+      >
+        <div className="h-full overflow-auto pr-1">
+          <BorrowerDetail />
+        </div>
+      </section>
+
+      <section
+        className="
+          min-w-0 break-words
+          lg:max-h-[calc(100vh-10rem)] lg:overflow-hidden
+        "
+      >
+        <div className="h-full overflow-auto pr-1">
+          <BrokerOverview />
+        </div>
+      </section>
     </div>
   )
 }
